342d709dd3760f59257ac6a038e1f1ca287a2ae5
Xa Quan <quan.h.xa@gmail.com>
2026-03-10 03:47:10 +0000

    edit xacro to show link4.
    launch gz create node brigde, pose, joint, ...

--- assign1/launch/gz_sim.launch.py
+++ assign1/launch/gz_sim.launch.py
@@ -12,34 +12,94 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    package_name = 'assign1'
     ros_gz_sim_pkg_path = get_package_share_directory('ros_gz_sim')
-    example_pkg_path = FindPackageShare('assign1')  # Replace with your own package name
+    pkg_share = FindPackageShare(package_name)  # Replace with your own package name
+    
     gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
+    world_path = os.path.join('empty.sdf')
+    xacro_path = PathJoinSubstitution([pkg_share, 'urdf', 'scara.urdf.xacro'])
+
+    robot_description = ParameterValue(
+        Command(['xacro', ' ', xacro_path]),
+        value_type=str
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}],
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-name', 'scara_robot',
+            '-topic', 'robot_description',
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.2',
+        ],
+    )
+
+    joint_state_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/joint_states@sensor_msgs/msg/JointState@ignition.msgs.Model',
+        ],
+        output='screen'
+    )
+
+    joint_trajectory_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/model/scara_robot/joint_trajectory@trajectory_msgs/msg/JointTrajectory]ignition.msgs.JointTrajectory',
+        ],
+        output='screen'
+    )
+
+    pose_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/model/scara_robot/pose@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+        ],
+        output='screen'
+    )
 
     return LaunchDescription([
         SetEnvironmentVariable(
             'GZ_SIM_RESOURCE_PATH',
-            PathJoinSubstitution([example_pkg_path, 'models'])
+            PathJoinSubstitution([pkg_share, 'models'])
         ),
         SetEnvironmentVariable(
             'GZ_SIM_PLUGIN_PATH',
-            PathJoinSubstitution([example_pkg_path, 'plugins'])
+            PathJoinSubstitution([pkg_share, 'plugins'])
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(gz_launch_path),
             launch_arguments={
-                'gz_args': PathJoinSubstitution([example_pkg_path, 'worlds/example_world.sdf']),  # Replace with your own world file
+                'gz_args': world_path,  # Replace with your own world file
                 'on_exit_shutdown': 'True'
             }.items(),
         ),
+        robot_state_publisher,
+        joint_trajectory_bridge,
+        spawn_robot,
+        joint_state_bridge,
 
         # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
-        Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            arguments=['/example_imu_topic@sensor_msgs/msg/Imu@gz.msgs.IMU',],
-            remappings=[('/example_imu_topic',
-                         '/remapped_imu_topic'),],
-            output='screen'
-        ),
+        # Node(
+        #     package='ros_gz_bridge',
+        #     executable='parameter_bridge',
+        #     arguments=['/example_imu_topic@sensor_msgs/msg/Imu@gz.msgs.IMU',],
+        #     remappings=[('/example_imu_topic',
+        #                  '/remapped_imu_topic'),],
+        #     output='screen'
+        # ),
     ])
